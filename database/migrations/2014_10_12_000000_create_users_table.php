e67660e61da4166b1ba6951e1757e1c51a4c9387
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-09-09 13:15:15 +0300

    register and login and profile page

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string("token");
+            $table->integer("connection_id");
+            $table->enum("user_status", ["Offline", "Online"]);
+            $table->string("user_image");
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("users", function (Blueprint $table) {
+            $table->dropColumn("token");
+            $table->dropColumn("connection_id");
+            $table->dropColumn("user_status");
+            $table->dropColumn("user_image");
+        });
+    }
+}
